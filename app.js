b1eac0ad86eee83d938785842d30494e27490d80
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2018-08-21 11:17:21 -0500

    Completed the inheritance OOP drills in a bit of a southern fashion

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ a4.greet();
 a5.greet();
 */
 
+/*
 console.log('Part 3 with that sweet ES6 syntactical sugar:\n\n');
 
 class Animal {
@@ -94,3 +95,58 @@ a2.greet();
 a3.greet();
 a4.greet();
 a5.greet();
+*/
+
+class Vehicle {
+    constructor(make, wheels, doors) {
+        this.make = make;
+        this.wheels = wheels;
+        this.doors = doors;
+    }
+
+    aboutVehicle() {
+        console.log(`This right here ${this.make} has ${this.wheels} wheels and ${this.doors}, ya'hear?!`);
+    }
+}
+
+class Truck extends Vehicle {
+    constructor(make, wheels, doors, hasBed) {
+        super(make, wheels, doors);
+        this.hasBed = hasBed;
+    }
+    aboutVehicle() {
+        console.log(`This right here ${this.make} is a pick-em-up truck with ${this.wheels} wheels, and ${this.doors} doors. Does it have a bed, you ask? That's ${this.hasBed}, ya'hear?!`);
+    }
+}
+
+class Sedan extends Vehicle {
+    constructor(make, wheels, doors, size, mpg) {
+        super(make, wheels, doors);
+        this.size = size;
+        this.mpg = mpg;
+    }
+    aboutVehicle() {
+        console.log(`This right here ${this.make} is a see-dan with ${this.wheels} wheels, and ${this.doors} doors. It's ${this.size}-size, and gets ${this.mpg} MPG, ya'll!`);
+    }
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, wheels, doors, hasSteeringWheel, canReverse) {
+        super(make, wheels, doors);
+        this.hasSteeringWheel = hasSteeringWheel;
+        this.canReverse = canReverse;
+    }
+    aboutVehicle() {
+        console.log(`This right here ${this.make} is a motorcycle on account it's got just ${this.wheels} wheels, and ${this.doors} doors. If y'all think it's got a steering wheel, that's ${this.hasSteeringWheel} because it's got handlebars, and if you think it can go in reverse that's ${this.canReverse}!`);
+    }
+}
+
+let genericVehicle = new Vehicle('Mazda', 4, 4);
+let pickEmUp = new Truck('Ford', 6, 4, true);
+let myOldToyotaEcho = new Sedan('Toyota', 4, 2, 'Compact', 37);
+let badassCBR1000RR = new Motorcycle('Honda', 2, 0, false, false);
+
+genericVehicle.aboutVehicle();
+pickEmUp.aboutVehicle();
+myOldToyotaEcho.aboutVehicle();
+badassCBR1000RR.aboutVehicle();
